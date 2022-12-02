caea6a1b78fd7f9975211f498aeddae771ba5e44
misha-solntcev <misha.solntcev@gmail.com>
2022-12-02 13:27:45 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*14. Напечатать в возрастающем порядке все трехзначные числа, 
+    в десятичной записи которых нет одинаковых цифр. 
+    Операции деления, целочисленного деления и определения 
+    остатка не использовать.*/
+
+namespace Lab_3_2_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i <= 999; i++)
+            {
+                char[] digit = Convert.ToString(i).ToCharArray();                
+                if ((digit[0] == digit[1]) || (digit[1] == digit[2]) || (digit[0] == digit[2]))
+                    continue;
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
